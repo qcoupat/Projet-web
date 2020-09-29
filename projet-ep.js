b43dc5e30faefa8a5c89cb2eb24a7e5f49b0ca7d
Quentin Coupat <quentincoupat@gmail.com>
2020-09-29 14:04:23 +0200

    Ajout Javascript

---
+++ projet-ep.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('.text-langage',{delay:500, duration: 2000});
+ScrollReveal().reveal('.titre-langage');
+ScrollReveal().reveal('#logo-swift',{delay:500, duration: 2000});
+ScrollReveal().reveal('#logo-python',{delay:500, duration: 2000});
+ScrollReveal().reveal('#logo-javascript',{delay:500, duration: 2000});
+ScrollReveal().reveal('#html-css',{delay:500, duration: 2000});
