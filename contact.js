8af2e87906756c358b9f0804985abacb984365eb
Bensehilayoub <72084740+Bensehilayoub@users.noreply.github.com>
2020-09-29 16:13:44 +0200

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('h1', { delay: 100, distance: '500px', origin:'left'});   
+ScrollReveal().reveal('.contact', {delay : 500, distance: '500px', origin:'left'});
+ScrollReveal().reveal('.formulaire', {delay : 1000, distance: '500px', origin:'right'});
+ScrollReveal().reveal('.Suiveznous', {delay : 1000, distance: '500px', origin:'bottom'});
+
